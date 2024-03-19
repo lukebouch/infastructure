9d65da53ba84878d6fba3f5f331e6a7ba82fe11f
Luke Bouch <luke@lukebouch.com>
2024-03-19 16:06:34 -0400

    Add Immich backup script

---
+++ bumble-bee/scripts/backup-immich.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+b2_url="b2://$BACKBLAZE_ID:$BACKBLAZE_KEY@lb-backups/duplicity/bumble-bee"
+
+cd /media/data/photos
+
+duplicity backup --no-encryption library "${b2_url}/immich/library"
+duplicity backup --no-encryption upload "${b2_url}/immich/upload"
+duplicity backup --no-encryption profile "${b2_url}/immich/profile"
