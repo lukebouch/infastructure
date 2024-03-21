713223a0272c83ce461223525f9770e1f3155c18
Luke Bouch <luke@lukebouch.com>
2024-03-21 18:19:26 -0400

    Update backup destination directory for Immich

--- bumble-bee/scripts/backup-immich.sh
+++ bumble-bee/scripts/backup-immich.sh
@@ -5,13 +5,13 @@ b2_url="b2://$BACKBLAZE_ID:$BACKBLAZE_KEY@lb-backups/duplicity/bumble-bee"
 cd /media/data/photos
 
 echo "Backing up library directory..."
-duplicity backup --no-encryption library "${b2_url}/immich/library"
+duplicity backup --no-encryption library "${b2_url}/apps/immich/library"
 
 echo "Backing up upload directory..."
-duplicity backup --no-encryption upload "${b2_url}/immich/upload"
+duplicity backup --no-encryption upload "${b2_url}/apps/immich/upload"
 
 echo "Backing up profile directory..."
-duplicity backup --no-encryption profile "${b2_url}/immich/profile"
+duplicity backup --no-encryption profile "${b2_url}/apps/immich/profile"
 
 # Create Database Dump
 echo "Creating database dump..."
@@ -21,4 +21,4 @@ sudo docker compose exec backup ./backup.sh
 # Backup Database Dump
 echo "Backing up lastest database dump..."
 cd /media/data/backups/immich/db_dumps/last
-duplicity backup --no-encryption immich-latest.sql.gz "${b2_url}/immich/database"
+duplicity backup --no-encryption immich-latest.sql.gz "${b2_url}/apps/immich/database"
