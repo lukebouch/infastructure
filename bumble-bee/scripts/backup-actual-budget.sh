cef22c762c34a4e41fd87f3e5836aefaa26709ae
Luke Bouch <luke@lukebouch.com>
2024-03-21 18:18:16 -0400

    Fix backup destination directory

--- bumble-bee/scripts/backup-actual-budget.sh
+++ bumble-bee/scripts/backup-actual-budget.sh
@@ -5,4 +5,4 @@ b2_url="b2://$BACKBLAZE_ID:$BACKBLAZE_KEY@lb-backups/duplicity/bumble-bee"
 cd /home/lbouch/apps/actual-budget
 
 echo "Backing up Actual Budget..."
-duplicity backup --no-encryption . "${b2_url}/actual-budget"
+duplicity backup --no-encryption . "${b2_url}/apps/actual-budget"
