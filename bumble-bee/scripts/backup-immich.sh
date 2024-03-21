884859346ed0828a1a55ae06c9c60a70f9fbe8d8
Luke Bouch <luke@lukebouch.com>
2024-03-21 18:01:09 -0400

    Update Immich backup script

--- bumble-bee/scripts/backup-immich.sh
+++ bumble-bee/scripts/backup-immich.sh
@@ -7,3 +7,7 @@ cd /media/data/photos
 duplicity backup --no-encryption library "${b2_url}/immich/library"
 duplicity backup --no-encryption upload "${b2_url}/immich/upload"
 duplicity backup --no-encryption profile "${b2_url}/immich/profile"
+
+# Backup Database Dump
+cd /media/data/backups/immich/db_dumps/last
+duplicity backup --no-encryption immich-latest.sql.gz "${b2_url}/immich/database"
