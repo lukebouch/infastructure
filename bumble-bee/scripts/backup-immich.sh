dd0f22a789e4becdcc6d3b801b25e2774e42bf68
Luke Bouch <luke@lukebouch.com>
2024-03-21 18:05:07 -0400

    Fix

--- bumble-bee/scripts/backup-immich.sh
+++ bumble-bee/scripts/backup-immich.sh
@@ -4,21 +4,21 @@ b2_url="b2://$BACKBLAZE_ID:$BACKBLAZE_KEY@lb-backups/duplicity/bumble-bee"
 
 cd /media/data/photos
 
-print "Backing up library directory..."
+echo "Backing up library directory..."
 duplicity backup --no-encryption library "${b2_url}/immich/library"
 
-print "Backing up upload directory..."
+echo "Backing up upload directory..."
 duplicity backup --no-encryption upload "${b2_url}/immich/upload"
 
-print "Backing up profile directory..."
+echo "Backing up profile directory..."
 duplicity backup --no-encryption profile "${b2_url}/immich/profile"
 
 # Create Database Dump
-print "Creating database dump..."
+echo "Creating database dump..."
 cd /home/lbouch/infastructure/bumble-bee/apps/immich
 sudo docker compose exec backup ./backup.sh
 
 # Backup Database Dump
-print "Backing up lastest database dump..."
+echo "Backing up lastest database dump..."
 cd /media/data/backups/immich/db_dumps/last
 duplicity backup --no-encryption immich-latest.sql.gz "${b2_url}/immich/database"
