391c9c3ce500b09fe278a5eaa66723175a838a47
Luke Bouch <luke@lukebouch.com>
2024-03-21 18:29:34 -0400

    Add Navidrome backup script

---
+++ bumble-bee/scripts/backup-navidrome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+b2_url="b2://$BACKBLAZE_ID:$BACKBLAZE_KEY@lb-backups/duplicity/bumble-bee"
+
+# Shutdown the app
+echo "Shutting down the Navidrome app..."
+cd /home/lbouch/infastructure/bumble-bee/apps/navidrome
+sudo docker compose down
+
+cd /home/lbouch/apps/navidrome
+
+echo "Backing up Navidrome..."
+duplicity backup --no-encryption . "${b2_url}/apps/navidrome"
+
+# Start up the app
+echo "Starting the Navidrome app back up..."
+cd /home/lbouch/infastructure/bumble-bee/apps/navidrome
+sudo docker compose up -d
