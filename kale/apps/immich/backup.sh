21212ad8b374922157ed1815b8a874684ecb3402
Luke Bouch <luke@lukebouch.com>
2024-10-10 14:46:02 -0400

    Backup Immich

---
+++ kale/apps/immich/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "We are backing up Immich."
+
+cd /home/lbouch/apps/immich
+
+echo "Backing up database dumps..."
+rsync -az --delete ./temp/db_dumps fm2211@fm2211.rsync.net:servers/kale/apps/immich/db_dumps
+
+echo "Backing up Immich library folder..."
+rsync -az --delete ./data/photos/library/library fm2211@fm2211.rsync.net:servers/kale/apps/immich/library
+
+echo "Backing up Immich upload folder..."
+rsync -az --delete ./data/photos/library/upload fm2211@fm2211.rsync.net:servers/kale/apps/immich/upload
+
+echo "Backing up Immich profile folder..."
+rsync -az --delete ./data/photos/library/profile fm2211@fm2211.rsync.net:servers/kale/apps/immich/profile
+
+echo "Done!"
