97c39a4663fafdb3d88582829222409b5111e88e
Luke Bouch <luke@lukebouch.com>
2024-10-11 09:53:53 -0400

    Fix

--- kale/apps/paperless/import-from-onedrive.sh
+++ kale/apps/paperless/import-from-onedrive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Moving files from OneDrive to the consume folder..."
-su lbouch rclone move "onedrive:Scanner Inbox" /home/lbouch/apps/paperless/consume -v > /dev/stdout 2>&1
+sudo -u lbouch rclone move "onedrive:Scanner Inbox" /home/lbouch/apps/paperless/consume -v > /dev/stdout 2>&1
 echo "Done."
